8c3619fd130533217a778eb293367a8f3a26f847
ameddin73 <ameddin73@gmail.com>
2020-04-12 23:33:53 -0400

    fix start_date required in db i can't believe i spent like 3 hours trying to figure out my validator when i'd just set the db up wrong good lord

---
+++ server/api/migrations/20200413033120-Program.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+    up: (queryInterface, Sequelize) => {
+        /*
+          Add altering commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+        */
+        return queryInterface.changeColumn(
+            'Programs',
+            'start_date',
+            {
+                allowNull: true,
+                type: Sequelize.DATE,
+            });
+    },
+
+    down: (queryInterface, Sequelize) => {
+        /*
+          Add reverting commands here.
+          Return a promise to correctly handle asynchronicity.
+
+          Example:
+          return queryInterface.dropTable('users');
+        */
+        return queryInterface.changeColumn(
+            'Programs',
+            'start_date',
+            {
+                allowNull: false,
+                type: Sequelize.DATE,
+            });
+    }
+};
