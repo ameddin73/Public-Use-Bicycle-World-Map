009127a0f05044c36c1c78f052a0a7c4c187be27
ameddin73 <ameddin73@gmail.com>
2020-04-13 11:58:49 -0400

    clean up home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,3 +15,21 @@ export default {
   }
 }
 </script>
+
+<style>
+    h1 {
+        position: fixed;
+        left: 50%;
+        transform: translateX(-50%);
+        color: aliceblue;
+        background: #2c3e50;
+        z-index: 10;
+        padding: 5px;
+        border-radius: 3px;
+        text-align: center;
+    }
+    home {
+        position: relative;
+        width: 100%;
+    }
+</style>
